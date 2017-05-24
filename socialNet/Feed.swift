dfe40074586d8c409ba5b2d556613a9fe854f42d
Truong Son Nguyen <sonic@Truongs-MacBook-Pro.local>
2017-05-24 10:13:37 +0700

    upload image

--- socialNet/Feed.swift
+++ socialNet/Feed.swift
@@ -13,9 +13,11 @@ import FirebaseDatabase
 class Feed: UIViewController , UITableViewDelegate , UITableViewDataSource , UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
     @IBOutlet weak var tableView:UITableView!
     @IBOutlet weak var addImage: CircleView!
+    @IBOutlet weak var captionField: FancyTextField!
     
     var posts = [Post]()
     var imagePicker:UIImagePickerController!
+    var imgSelected = false
     static var imagecache:NSCache<NSString,UIImage> = NSCache()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -74,6 +76,7 @@ class Feed: UIViewController , UITableViewDelegate , UITableViewDataSource , UII
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
             addImage.image = image
+            imgSelected = true
         }else{
             print(" A valid image wasnt selected")
         }
@@ -86,6 +89,29 @@ class Feed: UIViewController , UITableViewDelegate , UITableViewDataSource , UII
         
     }
     
+    @IBAction func postBtnTapped(_ sender: Any) {
+        guard let caption = captionField.text, caption != "" else {
+            print("JESS:Caption must be entered")
+            return
+        }
+        guard let img = addImage.image,imgSelected == true else {
+            print("JESSL: An Image must be selected")
+            return
+        }
+        if let imgData = UIImageJPEGRepresentation(img, 0.2){
+            let imgUid = NSUUID().uuidString
+            let metaData = FIRStorageMetadata()
+            metaData.contentType = "image/jpeg"
+            DataServices.ds.REF_POST_IMAGEs.child(imgUid).put(imgData,metadata:metaData) {(metadata,error) in
+                if error != nil {
+                    print("JESS:Unable to upload image from Firebase Storage")
+                } else {
+                    print("JESS:Successfully uploaded")
+                    let downloadURL = metaData.downloadURL()?.absoluteString
+            }
+        }
+    }
+}
     @IBAction func SignOutTapped(_ sender: Any) {
        let keychainResult =  KeychainWrapper.standard.removeObject(forKey: Key_UID)
         print("JESS: \(keychainResult)")
